7885191a0471f2699083d4dea25a040e5005b25b
Box-o <jeffmailkr@gmail.com>
2023-12-27 20:20:16 +0900

    애니메이션 추가

--- app/src/main/java/com/example/sns/MainActivity.kt
+++ app/src/main/java/com/example/sns/MainActivity.kt
@@ -9,11 +9,13 @@ import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
-
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         /*
                 Handler(Looper.getMainLooper()).postDelayed({
@@ -31,7 +33,11 @@ class MainActivity : AppCompatActivity() {
         */
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val snackbar = Snackbar.make(findViewById(android.R.id.content), "공지 : 아주 특별 이벤트가 있습니다!! 자리로 가장 먼저 오시는 분께 소고기 오마카세!!", Snackbar.LENGTH_INDEFINITE)
+            val snackbar = Snackbar.make(
+                findViewById(android.R.id.content),
+                "공지 : 아주 특별 이벤트가 있습니다!! 자리로 가장 먼저 오시는 분께 소고기 오마카세!!",
+                Snackbar.LENGTH_INDEFINITE
+            )
             snackbar.setAction("닫기") {
                 snackbar.dismiss()
             }
@@ -39,32 +45,43 @@ class MainActivity : AppCompatActivity() {
         }, 5000)
 
         fun popUpSnackbar(view: View) {
-            val snackbar = Snackbar.make(findViewById(android.R.id.content), "공지 : 아주 특별 이벤트가 있습니다!! 자리로 가장 먼저 오시는 분께 소고기 오마카세!!", Snackbar.LENGTH_INDEFINITE)
+            val snackbar = Snackbar.make(
+                findViewById(android.R.id.content),
+                "공지 : 아주 특별 이벤트가 있습니다!! 자리로 가장 먼저 오시는 분께 소고기 오마카세!!",
+                Snackbar.LENGTH_INDEFINITE
+            )
             snackbar.setAction("닫기") {
                 snackbar.dismiss()
             }
             snackbar.show()
         }
+    }
+
         //클릭 리스너로 사용하는 방법을 생각해 볼 것
 
-        fun goToDetail(view: View) {
-            //Toast.makeText(view.context, "디테일 페이지로 이동합니다", Toast.LENGTH_SHORT).show()
+        /*
+                fun goToDetail(view: View) {
+                    //Toast.makeText(view.context, "디테일 페이지로 이동합니다", Toast.LENGTH_SHORT).show()
 
-            val intent = Intent(this, DetailActivity::class.java)
-            startActivity(intent)
-        }
-        fun goToSignIn(view: View) {
-            //Toast.makeText(view.context, "로그인 페이지로 이동합니다", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, DetailActivity::class.java)
+                    startActivity(intent)
+                }
+                fun goToSignIn(view: View) {
+                    //Toast.makeText(view.context, "로그인 페이지로 이동합니다", Toast.LENGTH_SHORT).show()
 
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-        }
+                    val intent = Intent(this, SignInActivity::class.java)
+                    startActivity(intent)
+                }
+                */
         fun goToMyPage(view: View) {
-            //Toast.makeText(view.context, "내 정보 페이지로 이동합니다", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(view.context, "로그인 페이지로 이동합니다", Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, MypageActivity::class.java)
             startActivity(intent)
+            overridePendingTransition(R.anim.get_in_trans, R.anim.get_out_trans)
         }
-    }
+
+
+
 }
 
